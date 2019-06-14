717dc78f4758dfee0ac6d80d461f0de35b4c80c6
Oleg Filimonov <Oleg.Filimonov@survalyzer.com>
2019-06-14 10:22:02 +0300

    init

--- PerfomanceTest/Program.cs
+++ PerfomanceTest/Program.cs
@@ -1,15 +1,118 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 
 namespace PerfomanceTest
 {
-    class Program
+    public class IntellGroupDatabasePerfTest
     {
-        static void Main(string[] args)
+        private readonly List<string> _queriesList = new List<string>();
+        private SqlConnection _connection;
+        private SqlCommand[] _commands;
+
+        [GlobalSetup]
+        public void GlobalInit()
         {
+            var allVariants = Queries.ColumnNames.Select(col => string.Format(Queries.GetDupItem, col)).ToArray();
+            _queriesList.AddRange(allVariants);
+        }
+
+        [IterationSetup(Target = nameof(IntellGroup_AfterCleanup))]
+        public void IterationSetupAfterCleanup()
+        {
+            _connection = new SqlConnection(@"Data Source=EFISCO-OFIL;Encrypt=False;Integrated Security=True;Initial Catalog=zSulaPNL_20180413104126_intelligencegroup_1eced1591d9743b5913cd95ffb7be73a");
+            _connection.Open();
+            _commands = _queriesList.Select(q => new SqlCommand(q, _connection)).ToArray();
+        }
+
+        [IterationSetup(Target = nameof(IntellGroup_BeforeCleanup))]
+        public void IterationSetupBeforeCleanup()
+        {
+            _connection = new SqlConnection(@"Data Source=EFISCO-OFIL;Encrypt=False;Integrated Security=True;Initial Catalog=zSulaPNL_20180413104126_intelligencegroup_1eced1591d9743b5913cd95ffb7be73a_copy");
+            _connection.Open();
+            _commands = _queriesList.Select(q => new SqlCommand(q, _connection)).ToArray();
+        }
+
+        [IterationSetup(Target = nameof(DemoDb))]
+        public void IterationSetupDemoDb()
+        {
+            Console.WriteLine("Start local B");
+            _connection = new SqlConnection(@"Data Source=EFISCO-OFIL;Encrypt=False;Integrated Security=True;Initial Catalog=zClient_20190603143232_demo_0053e79d0981494a86b927f5cad88fe7");
+            _connection.Open();
+            _commands = _queriesList.Select(q => new SqlCommand(q, _connection)).ToArray();
+            Console.WriteLine("End local B");
+        }
+
+        [IterationCleanup]
+        public void IterationCleanup()
+        {
+            foreach (var command in _commands)
+            {
+                command.Dispose();
+            }
+
+            _connection.Close();
+            _commands = null;
+            _connection = null;
+        }
+
+        [Benchmark]
+        public void IntellGroup_BeforeCleanup()
+        {
+            foreach (var command in _commands)
+            {
+                using (var reader = command.ExecuteReader())
+                {
+                    //while (reader.Read())
+                    //{
+
+                    //}
+                }
+            }
+        }
+
+        [Benchmark(Baseline = true)]
+        public void IntellGroup_AfterCleanup()
+        {
+            foreach (var command in _commands)
+            {
+                using (var reader = command.ExecuteReader())
+                {
+                    //while (reader.Read())
+                    //{
+
+                    //}
+                }
+            }
+        }
+
+        [Benchmark]
+        public void DemoDb()
+        {
+            foreach (var command in _commands)
+            {
+                using (var reader = command.ExecuteReader())
+                {
+                    //while (reader.Read())
+                    //{
+
+                    //}
+                }
+            }
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<IntellGroupDatabasePerfTest>();
         }
     }
 }
---
+++ PerfomanceTest/Queries.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerfomanceTest
+{
+    class Queries
+    {
+        public static readonly string[] ColumnNames = {
+            "Question_Id_No_Answer"
+            ,"TextBlock_Id"
+            ,"Survey_Id_Congratulation"
+            ,"Survey_Id_Footer"
+            ,"Survey_Id_Header"
+            ,"Variable_Id"
+            ,"VariableCharacteristic_Id"
+            ,"MatrixRow_Autocomplete_Id"
+            ,"MatrixColumn_Autocomplete_Id"
+            ,"MatrixQuestionColumn_Id"
+            ,"MatrixColumnGroup_DropDownPrompt_Id"
+            ,"MatrixColumnGroup_Id_No_Answer"
+            ,"MatrixColumnGroup_Title_Id"
+            ,"RowRightLabel_Id"
+            ,"MatrixQuestionRow_Id"
+            ,"MultipleChoiceQuestionChoice_Autocomplete_Id"
+            ,"MultipleChoiceQuestionChoice_Id"
+            ,"RankOrderQuestionChoice_Autocomplete_Id"
+            ,"RankOrderQuestionChoice_Id"
+            ,"SingleChoiceQuestionChoice_Autocomplete_Id"
+            ,"SingleChoiceQuestionChoice_Id"
+            ,"CustomForward_Id"
+            ,"Question_Id_Question"
+            ,"Question_Id_Hint"
+            ,"DropDownQuestionChoice_Autocomplete_Id"
+            ,"DropDownQuestionChoice_Id"
+            ,"DropDownQuestion_Id"
+            ,"OpenQuestion_Id"
+            ,"SliderQuestionChoice_Autocomplete_Id"
+            ,"SliderQuestionChoice_Id"
+            ,"Content_MessageTemplate_Id"
+            ,"Subject_MessageTemplate_Id"
+            ,"SendEmail_Id_Message"
+            ,"SendEmail_Id_Subject"
+            ,"ValueAssignment_Id"
+            ,"ImplicitAssociationQuestionStimulus_Autocomplete_Id"
+            ,"ImplicitAssociationQuestionStimulus_Id"
+            ,"ImplicitAssociationQuestionId_DislikeButton"
+            ,"ImplicitAssociationQuestionId_LikeButton"
+            ,"OpenQuestion_Id_Placeholder"
+        };
+
+        public const string GetDupItem = @"
+SELECT st.{0}
+ ,st.Locale
+ ,COUNT(*)
+FROM SurveyText st
+WHERE st.{0} IS NOT NULL
+GROUP BY st.{0}
+        ,st.Locale";
+    }
+}
